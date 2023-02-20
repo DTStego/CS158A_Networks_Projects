0b4c7a2e4315f834d16d1162d4f539c13687be56
Jimmy Nguyen <dtstego@gmail.com>
2023-02-20 13:44:45 -0800

    Added sample and test code for assignment 2: HelloUDP.

---
+++ src/edu/sjsu/cs158a/HelloUDPServer.java
@@ -0,0 +1,17 @@
+package edu.sjsu.cs158a;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class HelloUDPServer
+{
+    public static void main(String[] args) throws SocketException
+    {
+        DatagramSocket socket = new DatagramSocket();
+    }
+
+    public static String receivePacket(DatagramSocket socket)
+    {
+        return "";
+    }
+}
