3fe6469f91c07413c1e999f0210952bede2a4faa
Jimmy Nguyen <dtstego@gmail.com>
2023-03-05 11:03:10 -0800

    Completed implementation of wikiUSA... Need to document code. Autograder not on for assignment yet.

--- src/edu/sjsu/cs158a/MyParser.java
+++ src/edu/sjsu/cs158a/MyParser.java
@@ -3,6 +3,11 @@ package edu.sjsu.cs158a;
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MyParser
@@ -10,20 +15,58 @@ public class MyParser
     static class MyParserCallback extends HTMLEditorKit.ParserCallback
     {
         static boolean isInTitle = false;
-        static boolean isInLink = false;
+        static String currentTitle;
 
         @Override
-        public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos)
+        public void handleStartTag(HTML.Tag tag, MutableAttributeSet a, int pos)
         {
-            if (t == HTML.Tag.TITLE)
+            if (tag == HTML.Tag.TITLE)
             {
                 isInTitle = true;
             }
 
-            if (t == HTML.Tag.A)
+            if (tag == HTML.Tag.A)
             {
-                if (a.getAttribute(HTML.Attribute.HREF) != null)
-                    isInLink = true;
+                String s = (String) a.getAttribute(HTML.Attribute.HREF);
+                if (s != null && s.startsWith("/wiki/"))
+                {
+                    // Check if it's the stop condition (Found the "Geographic_coordinate_system" page).
+                    if (s.equals("/wiki/".concat(Wiki.endPage)))
+                    {
+                        Wiki.isFound = true;
+                        System.out.println("Found in: " + currentTitle);
+                        System.exit(0);
+                    }
+                    // Add to the end of the child list.
+                    else
+                    {
+                        // No easy way to invalidate a link. Every link takes time to check so this is a very
+                        // archaic way of checking if it's a valid link based on output from wiki links.
+                        if (!s.contains(":") && !s.contains("%") && !s.contains(".") && !s.contains("#"))
+                        {
+                            // Add the part of the link after "/wiki/" to the childList in Wiki.java.
+                            Wiki.childList.add(s.substring(6));
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void handleText(char[] data, int pos)
+        {
+            if (isInTitle)
+            {
+                currentTitle = new String(data);
+            }
+        }
+
+        @Override
+        public void handleEndTag(HTML.Tag tag, int pos)
+        {
+            if (tag == HTML.Tag.TITLE)
+            {
+                isInTitle = false;
             }
         }
     }
--- src/edu/sjsu/cs158a/Wiki.java
+++ src/edu/sjsu/cs158a/Wiki.java
@@ -4,19 +4,57 @@ import javax.swing.text.html.parser.ParserDelegator;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Wiki
 {
-    public static final ArrayList<String> childList = new ArrayList<>();
+    // HashSet containing child links (only one layer) that are in the first page.
+    public static final HashSet<String> childList = new HashSet<>();
+
+    // If the specific page for "Geographic_coordinate_system" is found, end the program using this boolean.
+    public static boolean isFound = false;
     public static final String wikipediaPrefix = "https://www.wikipedia.com/wiki/";
+    public static final String endPage = "Geographic_coordinate_system";
 
 
     public static void main(String[] args) throws IOException
     {
+        if (args.length < 1)
+        {
+            System.err.println("Please input a string containing the portion of the Wikipedia link after /wiki/");
+            System.exit(2);
+        }
+
+        System.out.println("Searching: " + args[0].replaceAll("_", " ") + " - Wikipedia");
+
+        // Case for if the argument matches the endPage.
+        if (args[0].equals(endPage))
+        {
+            System.out.println("Found in: " + args[0].replaceAll("_", " ") + " - Wikipedia");
+            System.exit(0);
+        }
+
+        // Scrape the first page.
         String webpageContent = webScrape(args[0]);
 
         ParserDelegator delegator = new ParserDelegator();
+
+        // Parse the information on the page, looking for content based on MyParser.java.
+        // The variable "isFound" will change if the parser finds "endPage". Otherwise, the "childList" is filled.
         delegator.parse(new StringReader(webpageContent), new MyParser.MyParserCallback(), true);
+
+        // If "endPage" was not found in the first page, check the pages in "childList"
+        System.out.println("Checking children:");
+
+        // Store contents of childList in an ArrayList so the Parser can't add elements, perhaps messing with iteration.
+        ArrayList<String> tempList = new ArrayList<>(childList);
+
+        for (String s : tempList)
+        {
+            // Scrape the page and parse.
+            webpageContent = webScrape(s);
+            delegator.parse(new StringReader(webpageContent), new MyParser.MyParserCallback(), true);
+        }
     }
 
     /**
@@ -35,7 +73,7 @@ public class Wiki
             InputStream inputStream = wikipediaURL.openStream();
 
             // Reads 5120 bytes at a time from the page.
-            byte[] webpageData = new byte[5120];
+            byte[] webpageData = new byte[10240];
 
             int rc = 0;
 
@@ -51,7 +89,7 @@ public class Wiki
         catch (FileNotFoundException e)
         {
             System.out.println("Could not search: " + wikipediaPrefix.concat(webpageName));
-            System.exit(2);
+            System.exit(1);
         }
         catch (IOException e)
         {
